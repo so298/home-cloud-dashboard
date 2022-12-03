bdcac0e4fe09b3e1e1def892de85e71fefaaa2d5
Sosuke Hosokawa <so298go@gmail.com>
2022-12-03 23:58:04 +0900

    add wol api

--- src/api/api.ts
+++ src/api/api.ts
@@ -2,6 +2,7 @@ import axios from "axios";
 import { apiUrl } from "./url";
 
 export type Device = {
+  id: number;
   name: string;
   mac: string;
 };
@@ -9,4 +10,9 @@ export type Device = {
 export const getDevices = async () => {
   const devices = await axios.get(`${apiUrl}/devices`);
   return devices.data as Device[];
+};
+
+export const callWol = async (device: Device) => {
+  const res = await axios.post(`${apiUrl}/devices/wol`, device);
+  return res;
 };
--- src/pages/Devices.tsx
+++ src/pages/Devices.tsx
@@ -1,7 +1,19 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Typography, Divider } from "@mui/material";
+import { DataGrid, GridColDef, GridValueGetterParams } from "@mui/x-data-grid";
 import React, { useEffect, useState } from "react";
-import { Device, getDevices } from "../api/api";
+import { callWol, Device, getDevices } from "../api/api";
 import { Frame } from "../components/Frame";
+
+const columns: GridColDef[] = [
+  { field: "id", headerName: "ID", width: 70 },
+  { field: "name", headerName: "Device name", width: 200 },
+  { field: "mac", headerName: "MAC address", width: 160 },
+  {
+    field: "status",
+    headerName: "Status",
+    width: 160,
+  },
+];
 
 export const Devices = () => {
   const [devices, setDevices] = useState<Device[]>([]);
@@ -15,10 +27,27 @@ export const Devices = () => {
 
   return (
     <Frame>
-      <Box>
+      <Box sx={{ height: 400, width: 700 }}>
         <Typography variant="h2" noWrap component="div">
           Devices
         </Typography>
+        <DataGrid
+          rows={devices}
+          columns={columns}
+          pageSize={5}
+          rowsPerPageOptions={[5]}
+          onRowClick={(data) => {
+            const callDevice = data.row as Device;
+            if (window.confirm(`Do you wake up device "${callDevice.name}"?`)) {
+              callWol(callDevice)
+                .then((res) => {
+                  console.info("[wol success]");
+                  console.info(res);
+                })
+                .catch((error) => console.error(error));
+            }
+          }}
+        />
       </Box>
     </Frame>
   );
